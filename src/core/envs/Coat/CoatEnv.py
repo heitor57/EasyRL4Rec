d72b681703b0c503ab27c3b152696755e34e882e
heitor57 <heitorwerneck@hotmail.com>
2025-01-14 13:27:54 -0300

    coat dataset revert

--- src/core/envs/Coat/CoatEnv.py
+++ src/core/envs/Coat/CoatEnv.py
@@ -4,15 +4,15 @@ import numpy as np
 
 sys.path.extend([".", "./src", "./src/DeepCTR-Torch", "./src/tianshou"])
 from src.core.envs.BaseEnv import BaseEnv
-from src.core.envs.SiTunes.SiTunesData import SiTunesData
+from src.core.envs.Coat.CoatData import CoatData
 
 # ROOTPATH = os.path.dirname(__file__)
-ROOTPATH = "data/SiTunes"
+ROOTPATH = "data/Coat"
 DATAPATH = os.path.join(ROOTPATH, "data_raw")
 PRODATAPATH = os.path.join(ROOTPATH, "data_processed")
 
 
-class SiTunesEnv(BaseEnv):
+class CoatEnv(BaseEnv):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, mat=None, df_item=None, mat_distance=None, 
@@ -24,13 +24,13 @@ class SiTunesEnv(BaseEnv):
         else:
             self.mat, self.df_item, self.mat_distance = self.load_env_data()
 
-        super(SiTunesEnv, self).__init__(num_leave_compute, leave_threshold, max_turn, random_init)
+        super(CoatEnv, self).__init__(num_leave_compute, leave_threshold, max_turn, random_init)
 
     @staticmethod
     def load_env_data():
-        mat = SiTunesData.load_mat()
-        df_item = SiTunesData.load_item_feat()
-        mat_distance = SiTunesData.get_saved_distance_mat(mat, PRODATAPATH)
+        mat = CoatData.load_mat()
+        df_item = CoatData.load_item_feat()
+        mat_distance = CoatData.get_saved_distance_mat(mat, PRODATAPATH)
         return mat, df_item, mat_distance
 
     def _determine_whether_to_leave(self, t, action):
